10bae957265604162ede0b84c7bd7430538fda79
fancn <fanyj@yikeer.com>
2017-06-24 21:50:04 +0800

    handlebars enabled

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const express = require('express')
-
 const app = express()
+const engines = require('consolidate')
+
+app.engine('hbs', engines.handlebars)
+app.set('views', './views')
+app.set('view engine', 'hbs')
+
+const users = [
+  {
+    gender: 'female',
+    name: {
+      title: 'miss',
+      first: 'mary',
+      last: 'jones',
+    },
+    email: 'mary.jones56@example.com',
+    username: 'crazypeacock512',
+  },
+  {
+    gender: 'male',
+    name: {
+      title: 'mr',
+      first: 'alan',
+      last: 'walters',
+    },
+    email: 'alan.walters29@example.com',
+    username: 'crazytiger134',
+  },
+]
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!!!!')
+  res.render('index', { users })
 })
 
 app.get('/foo', (req, res) => {
